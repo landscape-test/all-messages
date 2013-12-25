05696b9f2e2c41ca891a8595a911a6fc5c786d5a
carlio <git@carlcrowder.com>
2013-12-25 12:16:12 +0000

    Some extra files to help test if files are ignored correctly

---
+++ setup.py
@@ -0,0 +1,5 @@
+
+# this should actually be ignored by prospector
+
+a = 1
+b = 3 / 0
---
+++ tests/test_nothing.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class TestNothing(TestCase):
+
+    def nothing_test(self):
+        fish = 'cake'
+        print 'hello'
